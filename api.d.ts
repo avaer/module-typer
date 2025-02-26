3028052a094471caca148644c5526487163ca55f
Avaer Kazmer <ultimus@gmail.com>
2025-02-26 12:49:07 -0700

    Declare more modules

--- api.d.ts
+++ api.d.ts
@@ -1,1 +1,5 @@
 declare module 'module-typer';
+declare module 'module-typer/lib/loaders/loaders.mjs';
+declare module 'module-typer/lib/loaders/file-loader.mjs';
+declare module 'module-typer/lib/loaders/github-loader.mjs';
+declare module 'module-typer/lib/loaders/octokit-loader.mjs';
